a6cb36585e3321df48ea8301652a15239d7fb833
frenzie24 <kevincharlesgross@gmail.com>
2024-05-24 01:19:29 -0400

    added async to product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  res.status(200).json({msg: 'hi dickbutt'});
   // find a single product by its `id`
+  //const product = await 
   // be sure to include its associated Category and Tag data
 });
 
@@ -48,7 +50,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -92,7 +94,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
 });
 
