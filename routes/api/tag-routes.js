5079d2be5e71b6482edf7280f25416ac6c4674ab
frenzie24 <kevincharlesgross@gmail.com>
2024-05-24 23:26:08 -0400

    added logic to get all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
-
+const {log, error} = require('@frenzie24/logger')
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    log(`Getting all products:`, 'blue');
+    const tags = await Tag.findAll({ include: [{ model: Product, through: ProductTag }] });
+    if (!tags) {
+      warn('We had an issue getting products.');
+      res.status(404).json({ msg: 'there was an issue getting all products' });
+    }
+    res.status(200).json(tags);
+  } catch (e) {
+    // if we have an error, have logger print a formatted error;
+    error(e);
+    res.status(500).json(e);
+  }
 });
 
 router.get('/:id', (req, res) => {
